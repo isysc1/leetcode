08cc301dd53d7a7b5f7e726dd0267fe18e0a8cb2
isysc1 <isysc1@163.com>
2020-03-07 10:40:26 +0800

    [develop 03-08 iysc] add question

---
+++ src/OfferGogogo/Solution14_2.java
@@ -0,0 +1,21 @@
+package OfferGogogo;
+
+/**
+ * @author isysc1@163.com
+ * @date 2020/3/7
+ */
+public class Solution14_2 {
+    public int cuttingRope(int n) {
+        if(n == 2)
+            return 1;
+        if(n == 3)
+            return 2;
+        long res = 1;
+        while(n > 4){
+            res *= 3;
+            res = res % 1000000007;
+            n -= 3;
+        }
+        return (int)(res * n % 1000000007);
+    }
+}
