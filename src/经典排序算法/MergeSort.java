31e47127f67497e032620914764c2b6db3c1011d
isysc1 <isysc1@163.com>
2020-03-14 15:08:03 +0800

    [develop 03-14 isysc] update question

--- src/经典排序算法/MergeSort.java
+++ src/经典排序算法/MergeSort.java
@@ -5,4 +5,81 @@ package 经典排序算法;
  * @date 2020/3/13
  */
 public class MergeSort {
+    /**
+     * 归并排序
+     * @param arr
+     */
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m + 1;
+        while (p1 <= m && p2 <= r) {
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+    }
+
+//    拓展问题：求最小和 [1,4,6,9,7,7] 求自己数的左边比自己小的数
+//    public static int smallSum(int[] arr) {
+//        if (arr == null || arr.length < 2) {
+//            return 0;
+//        }
+//        return mergeSort(arr, 0, arr.length - 1);
+//    }
+//
+//    public static int mergeSort(int[] arr, int l, int r) {
+//        if (l == r) {
+//            return 0;
+//        }
+//        int mid = l + ((r - l) >> 1);
+//        return mergeSort(arr, l, mid) + mergeSort(arr, mid + 1, r) + merge(arr, l, mid, r);
+//    }
+//
+//    public static int merge(int[] arr, int l, int m, int r) {
+//        int[] help = new int[r - l + 1];
+//        int i = 0;
+//        int p1 = l;
+//        int p2 = m + 1;
+//        int res = 0;
+//        while (p1 <= m && p2 <= r) {
+//            res += arr[p1] < arr[p2] ? (r - p2 + 1) * arr[p1] : 0;
+//            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+//        }
+//        while (p1 <= m) {
+//            help[i++] = arr[p1++];
+//        }
+//        while (p2 <= r) {
+//            help[i++] = arr[p2++];
+//        }
+//        for (i = 0; i < help.length; i++) {
+//            arr[l + i] = help[i];
+//        }
+//        return res;
+//    }
 }
