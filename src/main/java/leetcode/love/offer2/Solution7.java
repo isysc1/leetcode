061ee91b66ca4d150157250bc4acff33006eb09f
杨事昌 <yryangsc25519@yunrong.cn>
2022-11-23 21:15:20 +0800

    feat: 剑指 Offer 07. 重建二叉树

---
+++ src/main/java/leetcode/love/offer2/Solution7.java
@@ -0,0 +1,93 @@
+package leetcode.love.offer2;
+
+//输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
+//
+// 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+//
+//
+//
+// 示例 1:
+//
+//
+//Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//Output: [3,9,20,null,null,15,7]
+//
+//
+// 示例 2:
+//
+//
+//Input: preorder = [-1], inorder = [-1]
+//Output: [-1]
+//
+//
+//
+//
+// 限制：
+//
+// 0 <= 节点个数 <= 5000
+//
+//
+//
+// 注意：本题与主站 105 题重复：https://leetcode-cn.com/problems/construct-binary-tree-from-
+//preorder-and-inorder-traversal/
+// Related Topics 树 数组 哈希表 分治 二叉树
+// 👍 940 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.HashMap;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution7 {
+    HashMap<Integer, Integer> map = new HashMap<>();//标记中序遍历
+    int[] preorder;//保留的先序遍历，方便递归时依据索引查看先序遍历的值
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        //将中序遍历的值及索引放在map中，方便递归时获取左子树与右子树的数量及其根的索引
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        //三个索引分别为
+        //当前根的的索引
+        //递归树的左边界，即数组左边界
+        //递归树的右边界，即数组右边界
+        return recur(0, 0, inorder.length - 1);
+    }
+
+    TreeNode recur(int pre_root, int in_left, int in_right) {
+        if (in_left > in_right) {
+            return null;// 相等的话就是自己
+        }
+        TreeNode root = new TreeNode(preorder[pre_root]);//获取root节点
+        int idx = map.get(preorder[pre_root]);//获取在中序遍历中根节点所在索引，以方便获取左子树的数量
+        //左子树的根的索引为先序中的根节点+1
+        //递归左子树的左边界为原来的中序in_left
+        //递归左子树的右边界为中序中的根节点索引-1
+        root.left = recur(pre_root + 1, in_left, idx - 1);
+        //右子树的根的索引为先序中的 当前根位置 + 左子树的数量 + 1
+        //递归右子树的左边界为中序中当前根节点+1
+        //递归右子树的右边界为中序中原来右子树的边界
+        root.right = recur(pre_root + (idx - in_left) + 1, idx + 1, in_right);
+        return root;
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
