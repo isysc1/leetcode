ff5a4a0d0965dcebd768a7ced734ee3a82254e33
杨事昌 <yryangsc25519@yunrong.cn>
2022-12-05 23:09:13 +0800

    feat:算法更新

--- src/main/java/leetcode/love/offer2/Solution14_1.java
+++ src/main/java/leetcode/love/offer2/Solution14_1.java
@@ -29,14 +29,19 @@ package leetcode.love.offer2;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution14_1 {
     public int cuttingRope(int n) {
-        int[] dp = new int[n + 1];
-        dp[2] = 1;
-        for(int i = 3; i < n + 1; i++){
-            for(int j = 2; j < i; j++){
-                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+        int[] dp = new int[n+1];
+        if(n <= 3) {
+            return n-1;
+        }
+        dp[1] = 1;dp[2] = 2;dp[3] = 3;
+        for(int i = 4; i <= n; i++) {
+            for(int j = 1; j <= i / 2; j++) {
+                dp[i] = Math.max(dp[i],dp[i-j]*dp[j]);
             }
         }
         return dp[n];
     }
 }
+
+// i / 2 后在分解就不会有大数了 其实
 //leetcode submit region end(Prohibit modification and deletion)
